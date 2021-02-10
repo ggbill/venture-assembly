c5f19e81dedfb7024f0a045466b2dbbb3ee6588d
ggbill <bill@goinggone.io>
2021-02-10 18:32:50 +0000

    typing text

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,7 +1,8 @@
 import './home.scss'
 import React from 'react'
 import MenuBar from '../shared/MenuBar'
-import { Link } from 'react-router-dom'
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 
 
 const Home = () => {
@@ -12,11 +13,31 @@ const Home = () => {
                 <div className="content">
                     <div className="title-wrapper">
                         <span className="title">Venture Assembly<span className="full-stop">.</span></span>
+                        <div className="sub-title"><span>Where startups come to&nbsp;</span>
+                            <div className="typing-text-wrapper">
+                                <Typist>
+                                    <Typist.Delay ms={4000} />
+                                    <span>plan funding rounds<span className="full-stop">.</span></span>
+                                    <Typist.Backspace count={20} delay={2000} />
+                                    <Typist.Delay ms={1000} />
+                                    <span>find specialist help<span className="full-stop">.</span></span>
+                                    <Typist.Backspace count={21} delay={2000} />
+                                    <Typist.Delay ms={1000} />
+                                    <span>build their network<span className="full-stop">.</span></span>
+                                    <Typist.Backspace count={21} delay={2000} />
+                                    <Typist.Delay ms={1000} />
+                                    <span>hire staff<span className="full-stop">.</span></span>
+                                    <Typist.Backspace count={11} delay={2000} />
+                                    <Typist.Delay ms={1000} />
+                                    <span><b>wage war on startup bloat<span className="full-stop">.</span></b></span>
+                                </Typist>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="read-more">
                     read more
-                    <div className="arrow">↓</div>
+                <div className="arrow">↓</div>
                 </div>
             </section>
 
