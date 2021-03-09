06b75044282e8844c85995940d38bda3a8591b01
ggbill <bill@goinggone.io>
2021-03-09 13:01:20 +0000

    secure google api key

--- frontend/src/hooks/useYoutubeApi.tsx
+++ frontend/src/hooks/useYoutubeApi.tsx
@@ -1,29 +1,18 @@
-const key = "AIzaSyBIuxy5ZyltcojVbkvk84z9ycSz4TrUcOU"
+const key = process.env.REACT_APP_GOOGLE_API_KEY || ""
 
 const useYoutubeApi = () => {
 
     const getVideosInPlaylist = async (playlistId: string) => {
-        // const url = "https://www.googleapis.com/youtube/v3/channels?id=UCHvoyoD-8_RuVPDowvroAUg&key=AIzaSyBIuxy5ZyltcojVbkvk84z9ycSz4TrUcOU&part=snippet,contentDetails,statistics,status"
         const url = "https://www.googleapis.com/youtube/v3/playlistItems?" + new URLSearchParams({
             key: key,
             part: 'snippet,contentDetails',
             playlistId: playlistId,
-            // playlistId: "PLQCLYF55tnNNKg4BuzRN2oZ1wMqYc9knF",
             order: "date",
             maxResults: "50"
 
         })
 
-
-        const options = {
-            // method: "GET",
-            // headers: {
-            //     'Accept': 'application/json',
-            //     'Content-Type': 'application/json'
-            // }
-        }
-
-        return fetch(url, options)
+        return fetch(url)
             .then(response => {
                 return response.json()
             })
@@ -33,36 +22,25 @@ const useYoutubeApi = () => {
                     videoIds += `${element.snippet.resourceId.videoId},`
                 });
 
-                // console.log(videoIds)
                 return getVideoDetails(videoIds)
             })
             .catch(err => {
+                console.log(err)
                 throw new Error(err);
             });
     }
 
     const getVideoDetails = async (csvVideoIds: string) => {
-        // const url = "https://www.googleapis.com/youtube/v3/channels?id=UCHvoyoD-8_RuVPDowvroAUg&key=AIzaSyBIuxy5ZyltcojVbkvk84z9ycSz4TrUcOU&part=snippet,contentDetails,statistics,status"
         const url = "https://www.googleapis.com/youtube/v3/videos?" + new URLSearchParams({
             key: key,
-            part: 'snippet,contentDetails',
+            part: 'snippet,contentDetails,statistics',
             id: csvVideoIds,
-            // playlistId: "PLQCLYF55tnNNKg4BuzRN2oZ1wMqYc9knF",
             order: "date",
             maxResults: "50"
 
         })
 
-
-        const options = {
-            // method: "GET",
-            // headers: {
-            //     'Accept': 'application/json',
-            //     'Content-Type': 'application/json'
-            // }
-        }
-
-        return fetch(url, options)
+        return fetch(url)
             .then(response => response.json())
             .then(json => {
                 // console.log(json)
